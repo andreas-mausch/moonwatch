fc7dea8a331fccc046aceae9dcf9c269fd37d4c3
neonew <neonew@gmail.com>
2020-06-17 01:40:29 +0200

    Add all background offsets

--- js/app.js
+++ js/app.js
@@ -28,8 +28,34 @@
     }
 
     function moonPhase() {
+        var positions = [
+                         "-1110px -1840px",
+                         "-45px -50px",
+                         "-405px -50px",
+                         "-760px -50px",
+                         "-1120px -50px",
+                         "-35px -410px",
+                         "-390px -410px",
+                         "-750px -410px",
+                         "-1110px -410px",
+                         "-35px -1840px",
+                         "-390px -1840px",
+                         "-750px -1840px",
+                         "-35px -1480px",
+                         "-390px -1480px",
+                         "-750px -1480px",
+                         "-1110px -1480px",
+                         "-35px -770px",
+                         "-390px -770px",
+                         "-750px -770px",
+                         "-1110px -770px",
+                         "-35px -1125px",
+                         "-390px -1125px",
+                         "-750px -1125px",
+                         "-1110px -1125px",
+        ];
         var element = document.querySelector("#background-moon");
-        element.style.backgroundPosition = "65% 59.5%";
+        element.style.backgroundPosition = positions[0];
     }
 
     /**
